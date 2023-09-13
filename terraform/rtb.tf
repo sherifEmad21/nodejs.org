933e4f2bf7cec67fbe986b1f745106d07a4430c1
sherifEmad21 <sherif.emad1091@gmail.com>
2023-09-13 22:46:33 +0300

    modifications

--- terraform/rtb.tf
+++ terraform/rtb.tf
@@ -12,30 +12,30 @@ resource "aws_route_table" "public_route_table" {
     }
 }
 
-# #  route table responsible for routing traffic to the subnets
-# resource "aws_route_table" "private_route_table-1" {
-#     vpc_id = aws_vpc.test-vpc.id
+#  route table responsible for routing traffic to the subnets
+resource "aws_route_table" "private_route_table-1" {
+    vpc_id = aws_vpc.test-vpc.id
 
-#     route {
-#         cidr_block = "0.0.0.0/0"
-#         gateway_id = aws_nat_gateway.gw1.id
-#     }
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_nat_gateway.gw1.id
+    }
 
-#     tags = {
-#         Name: "private-rtb-1"
-#     }
-# }
+    tags = {
+        Name: "private-rtb-1"
+    }
+}
 
-# #  route table responsible for routing traffic to the subnets
-# resource "aws_route_table" "private_route_table-2" {
-#     vpc_id = aws_vpc.test-vpc.id
+#  route table responsible for routing traffic to the subnets
+resource "aws_route_table" "private_route_table-2" {
+    vpc_id = aws_vpc.test-vpc.id
 
-#     route {
-#         cidr_block = "0.0.0.0/0"
-#         gateway_id = aws_nat_gateway.gw2.id
-#     }
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_nat_gateway.gw2.id
+    }
 
-#     tags = {
-#         Name: "private-rtb-2"
-#     }
-# }
+    tags = {
+        Name: "private-rtb-2"
+    }
+}
--- terraform/sg.tf
+++ terraform/sg.tf
@@ -73,33 +73,6 @@ resource "aws_security_group" "test-sg" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
-    ingress {
-        description = "Allow incoming traffic from the EKS control plane"
-        from_port   = 1025
-        to_port     = 65535
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-        description = "Allow incoming traffic for Kubernetes pods within the VPC"
-        from_port   = 0
-        to_port     = 65535
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-        description = "Allow incoming traffic from Amazon ECR"
-        from_port   = 443
-        to_port     = 443
-        protocol    = "tcp"
-        # Replace with the CIDR range(s) used by your ECR repository or ECR registry.
-        # Example: cidr_blocks = ["203.0.113.0/24"]
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-
     # egress --> outbound filter
     egress {
         from_port   = 0
