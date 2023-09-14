6efa5ab2dffb1e577f2eea2cfcdadd01ed3b41ff
sherifEmad21 <sherif.emad1091@gmail.com>
2023-09-14 22:04:16 +0300

    added insatnce profile and made some modifications

--- terraform/rtb-association.tf
+++ terraform/rtb-association.tf
@@ -9,12 +9,12 @@ resource "aws_route_table_association" "public-rtb-association-2" {
     route_table_id = aws_route_table.public_route_table.id
 }
 
-resource "aws_route_table_association" "private-rtb-association-1" {
-    subnet_id = aws_subnet.private-subnet-1.id 
-    route_table_id = aws_route_table.private_route_table-1.id
-}
+# resource "aws_route_table_association" "private-rtb-association-1" {
+#     subnet_id = aws_subnet.private-subnet-1.id 
+#     route_table_id = aws_route_table.private_route_table-1.id
+# }
 
-resource "aws_route_table_association" "private-rtb-association-2" {
-    subnet_id = aws_subnet.private-subnet-2.id 
-    route_table_id = aws_route_table.private_route_table-2.id
-}
+# resource "aws_route_table_association" "private-rtb-association-2" {
+#     subnet_id = aws_subnet.private-subnet-2.id 
+#     route_table_id = aws_route_table.private_route_table-2.id
+# }
